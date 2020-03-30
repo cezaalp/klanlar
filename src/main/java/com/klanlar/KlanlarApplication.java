f59509a4ce476211c65d648f6adfdac529e46872
alper.kurtoglu <alper.kurtoglu@intra.local>
2020-03-30 19:54:12 +0300

    COMMIT - 1

---
+++ src/main/java/com/klanlar/KlanlarApplication.java
@@ -0,0 +1,13 @@
+package com.klanlar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlanlarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlanlarApplication.class, args);
+	}
+
+}
